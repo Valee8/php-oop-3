e150945ea68b52004f260a891a379258509ae165
Vale <oirelav95@gmail.com>
2023-01-26 20:02:39 +0100

    aggiungo classe stipendio

---
+++ index.php
@@ -0,0 +1,66 @@
+<!-- ## Todo
+Creare una struttura di classi come segue. Creare qualche istanza per ogni classe, testandone la correttezza.
+Ogni classe dovra' inoltre avere il suo metodo `getHtml()` per la stmapa in pagina.
+
+### `Stipendio`
+#### Variabili
+- mensile
+- tredicesima (si/no)
+- quattordicesima (si/no)
+
+#### Metodi
+- metodo che restituisce lo stipendio annuale:
+`mensile * 12 + tredicesima + quattordicesima`
+**N.B.:** tredicesima e quattordicesima, se presenti, sono un mensile
+
+### `Persona`
+#### Variabili
+- nome
+- cognome
+- data di nascita
+- luogo di nascita
+- codice fiscale
+
+### `Impiegato`
+#### Variabili
+- nome
+- cognome
+- data di nascita
+- luogo di nascita
+- codice fiscale
+---
+- stipendio (classe `Stipendio`)
+- data di assunzione
+
+#### Metodi
+- metodo che restituisce lo stipendio annuale a partire dall'oggetto stipendio
+
+### `Capo`
+#### Variabili
+- nome
+- cognome
+- data di nascita
+- luogo di nascita
+- codice fiscale
+---
+- dividendo
+- bonus
+
+#### Metodi
+- metodo che restituisce il reddito annuale:
+`dividendo * 12 + bonus`
+
+### N.B.:
+La concentrazione va tutta sul rapporto tra:
+- `Persona` e `Impiegato`
+- `Persona` e `Capo`
+- `Impiegato` e `Stipendio` -->
+
+
+<?php
+
+    class Stipendio {
+        
+    }
+
+?>
